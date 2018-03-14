deb05983979d5f0b0ad5b9e27ebd082b49915d4c
Jarek Figura <jarek.figura@icloud.com>
2018-03-14 11:40:58 +0100

    squareDigit.js => square every digit of a number

---
+++ squareDigits.js
@@ -0,0 +1,31 @@
+/*
+  Square every digit of a number.
+  For example, if we run 9119 through the function, 811181 will come out,
+  because 92 is 81 and 12 is 1.
+
+  Note: The function accepts an integer and returns an integer
+ */
+
+function squareDigits (num){
+	//may the code be with you :)
+
+	num = Math.abs(num);
+	var digits = num.toString().split('').map(Number);
+	var str = "";
+
+	for (var i = 0; i < digits.length; i++) {
+		str += String(digits[i] * digits[i]);
+	}
+
+	return Number(str);
+}
+
+console.log(squareDigits(9119));
+console.log(squareDigits(0));
+console.log(squareDigits(1));
+console.log(squareDigits(00));
+console.log(squareDigits(-0));
+console.log(squareDigits(-00));
+console.log(squareDigits(-02));
+console.log(squareDigits(04));
+console.log(squareDigits(204507));
